e2edead1d9d8abaf853f54632f50ec07105a4aad
Najam <realnajmiter@gmail.com>
2025-03-18 13:01:14 +0500

    fix: code structure

---
+++ src/constants.ts
@@ -0,0 +1,52 @@
+export const fontDescriptions: { [key: string]: string } = {
+  'Fira Code': 'A monospace font with programming ligatures',
+  'JetBrains Mono': 'A typeface for developers',
+  'Source Code Pro':
+    'A monospaced font for user interfaces and coding environments',
+  'Cascadia Code': 'A fun, new monospaced font from Microsoft',
+  Hack: 'A typeface designed for source code',
+  Menlo: 'A monospaced font by Apple',
+  Monaco: 'A monospaced font by Apple',
+  Consolas: 'A monospaced font designed for Microsoft',
+  'Ubuntu Mono': 'A monospace font from the Ubuntu font family',
+  'Droid Sans Mono': 'A monospace font by Google Android',
+  'Roboto Mono': 'A monospaced version of Roboto',
+};
+
+export const fontDownloadUrls: { [key: string]: string } = {
+  'Fira Code':
+    'https://github.com/tonsky/FiraCode/releases/download/6.2/Fira_Code_v6.2.zip',
+  'JetBrains Mono':
+    'https://download.jetbrains.com/fonts/JetBrainsMono-2.304.zip',
+  'Source Code Pro':
+    'https://github.com/adobe-fonts/source-code-pro/releases/download/2.038R-ro%2F1.058R-it%2F1.018R-VAR/OTF-source-code-pro-2.038R-ro-1.058R-it.zip',
+  'Cascadia Code':
+    'https://github.com/microsoft/cascadia-code/releases/download/v2111.01/CascadiaCode-2111.01.zip',
+  'Ubuntu Mono':
+    'https://assets.ubuntu.com/v1/fad7939b-ubuntu-font-family-0.83.zip',
+  'Droid Sans Mono':
+    'https://github.com/sixrevisions/free-programming-fonts/blob/master/fonts/web/droid-sans-mono.ttf',
+  'Roboto Mono': 'https://fonts.google.com/download?family=Roboto%20Mono',
+  Menlo:
+    'https://github.com/hbin/top-programming-fonts/raw/refs/heads/master/Menlo-Regular.ttf',
+  Monaco:
+    'https://github.com/hbin/top-programming-fonts/raw/refs/heads/master/Monaco-Linux.ttf',
+  Hack: 'https://github.com/source-foundry/Hack/releases/download/v3.003/Hack-v3.003-ttf.zip',
+  Consolas:
+    'https://github.com/pensnarik/consolas-font/raw/refs/heads/master/Consolas-Regular.ttf',
+};
+
+export const fontVariations: { [key: string]: string[] } = {
+  'Cascadia Code': [
+    'Cascadia Code',
+    'CascadiaCode',
+    'Cascadia Mono',
+    'CascadiaMono',
+  ],
+  'JetBrains Mono': ['JetBrains Mono', 'JetBrainsMono'],
+  'Source Code Pro': ['Source Code Pro', 'SourceCodePro'],
+  'Ubuntu Mono': ['Ubuntu Mono', 'UbuntuMono'],
+  'Droid Sans Mono': ['Droid Sans Mono', 'DroidSansMono'],
+  'Roboto Mono': ['Roboto Mono', 'RobotoMono'],
+  'Fira Code': ['Fira Code', 'FiraCode'],
+};
